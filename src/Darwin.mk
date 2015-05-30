bc71ef8183541153f69fc822835ded48bd68f85a
Yibo Lin <yibolin@utexas.edu>
2015-05-30 17:03:37 -0500

    use O4 instead of O2 for release version

--- src/Darwin.mk
+++ src/Darwin.mk
@@ -14,7 +14,7 @@ endif
 
 CXXFLAGS_BASIC = -ferror-limit=1 -W -Wall -Wextra -Wreturn-type -ansi -m64 -Wno-deprecated
 CXXFLAGS_DEBUG = -g -DDEBUG $(CXXFLAGS_BASIC) 
-CXXFLAGS_RELEASE = -O2 -fopenmp $(CXXFLAGS_BASIC) 
+CXXFLAGS_RELEASE = -O4 -fopenmp $(CXXFLAGS_BASIC) 
 
 ARFLAGS = rvs
 
--- src/Unix.mk
+++ src/Unix.mk
@@ -18,6 +18,6 @@ endif
 
 CXXFLAGS_BASIC = -fmax-errors=1 -W -Wall -Wextra -Wreturn-type -ansi -m64 -Wno-deprecated
 CXXFLAGS_DEBUG = -g -DDEBUG $(CXXFLAGS_BASIC) 
-CXXFLAGS_RELEASE = -O2 -fopenmp $(CXXFLAGS_BASIC) 
+CXXFLAGS_RELEASE = -O4 -fopenmp $(CXXFLAGS_BASIC) 
 
 ARFLAGS = rvs
