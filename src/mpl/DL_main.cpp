1e681d877392e77595a8f368c7624c20a67374ab
sqPoseidon <qisunchn@gmail.com>
2018-08-14 22:43:48 +0800

    ...

--- src/mpl/DL_main.cpp
+++ src/mpl/DL_main.cpp
@@ -1,5 +1,8 @@
 #include"DL_MPL.h"
 
+// Use the API in SimpleMPL.h to create the Component file for dlx solver;
+// then call MPLD_Solver()
+
 int main()
 {
 	std::string InFileName = "karate.txt";
