e7cebf2d43731cea5548c8df4daa46147a5645ae
SUN Qi <qsun@linux10.cse.cuhk.edu.hk>
2018-10-17 23:26:38 +0800

    why same component different output

--- bin/run.sh
+++ bin/run.sh
@@ -22,13 +22,13 @@
 # if the benchmark contains polygon shapes, -shape must be set to POLYGON;
 # otherwise, set -shape to RECTANGLE is more memory efficient
 ./OpenMPL  \
-    -shape RECTANGLE \
-	-in /uac/gds/qsun/App/ISCAS_total/total_c4.gds  \
-    -out ./out.gds  \
-    -coloring_distance 120 \
-    -uncolor_layer  1 \
-	-uncolor_layer 101 \
-    -color_num 4 \
+    -shape POLYGON \
+	-in bench/total_c1.gds \
+    -out benchout/total_c1_out.gds \
+	-coloring_distance 120 \
+    -uncolor_layer 101 \
+    -uncolor_layer 1 \
+	-color_num 3 \
     -algo SDP \
     -thread_num 8 \
 	-simplify_level 3
