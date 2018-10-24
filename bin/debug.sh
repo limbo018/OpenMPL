451da28ab8abe9550d24482da5e3a27e513794b3
sqposeidon <qisunchn@gmail.com>
2018-10-25 00:19:52 +0800

    ..

--- bin/debug.sh
+++ bin/debug.sh
@@ -1,3 +1,3 @@
 file OpenMPL
-set args -shape POLYGON -in bench/total_c1.gds -out benchout/total_c1_out.gds -coloring_distance 120 -uncolor_layer 1 -uncolor_layer 101 -color_num 3 -algo ILP -thread_num 8 -stitch
+set args -shape POLYGON -in bench/total_c1.gds -out benchout/total_c1_out.gds -coloring_distance 120 -uncolor_layer 1 -uncolor_layer 101 -color_num 3 -algo SDP -thread_num 8 -stitch
 r
