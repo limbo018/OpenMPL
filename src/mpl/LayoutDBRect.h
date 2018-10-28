0220eb90f0bbe45b008efde119b77cb8fa6d8ec3
sqPoseidon <qisunchn@gmail.com>
2018-10-29 00:44:46 +0800

    virtual override

--- src/mpl/LayoutDBRect.h
+++ src/mpl/LayoutDBRect.h
@@ -38,6 +38,8 @@ struct LayoutDBRect : public LayoutDB
 	virtual void initialize_data();
     /// \return poly rect patterns 
     virtual std::vector<rectangle_pointer_type> const& polyrect_patterns() const {return vPatternBbox;}
+	/// \return poly rect begin Id, onyl used in LayoutDBPolygon, but also need to be implemented in LayoutDBRect
+	virtual std::vector<uint32_t> const& polyrectBgnId() const { std::vector<uint32_t> m; return m; }
     /// set color for patterns 
     /// \param pattern_id is the index of vPatternBbox
     virtual void set_color(uint32_t pattern_id, int8_t color);
